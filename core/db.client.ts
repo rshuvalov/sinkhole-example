1f8733879258ed30e2624fc1457d9450cece8ad8
rshuvalov <rshuvalov@rshuvalov-mbp.local>
2023-11-16 23:11:16 +0200

    init

---
+++ core/db.client.ts
@@ -0,0 +1,8 @@
+class DbClient {
+  exec<T>(sql: string): T {
+    // execute query
+    return {} as any;
+  }
+}
+
+export default DbClient;
---
+++ core/index.ts
@@ -0,0 +1,8 @@
+export { default as Controller } from './controller';
+
+export { default as Get } from './get';
+export { default as Post } from './post';
+export { default as Param } from './param';
+export { default as Body } from './body';
+
+export { default as DbClient } from './db.client';
